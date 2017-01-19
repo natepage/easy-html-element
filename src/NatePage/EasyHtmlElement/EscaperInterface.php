09762e1ec54c6cc8869828ed808503076b1e28e2
N@thix86 <nathan.page86@gmail.com>
2017-01-19 13:12:24 +0800

    [EscaperInterface] Added strategies setting/getting methods

--- src/NatePage/EasyHtmlElement/EscaperInterface.php
+++ src/NatePage/EasyHtmlElement/EscaperInterface.php
@@ -37,4 +37,74 @@ interface EscaperInterface
      * @return array
      */
     public function getUrlsAttributes();
+
+    /**
+     * Determine if html escaping strategy is applied.
+     *
+     * @param bool $escapeHtml
+     */
+    public function setEscapeHtml($escapeHtml = true);
+
+    /**
+     * Check if html escaping strategy is applied.
+     *
+     * @return bool
+     */
+    public function isEscapeHtml();
+
+    /**
+     * Determine if html attributes escaping strategy is applied.
+     *
+     * @param bool $escapeHtmlAttr
+     */
+    public function setEscapeHtmlAttr($escapeHtmlAttr = true);
+
+    /**
+     * Check if html attributes escaping strategy is applied.
+     *
+     * @return bool
+     */
+    public function isEscapeHtmlAttr();
+
+    /**
+     * Determine if css escaping strategy is applied.
+     *
+     * @param bool $escapeCss
+     */
+    public function setEscapeCss($escapeCss = true);
+
+    /**
+     * Check if css escaping strategy is applied.
+     *
+     * @return bool
+     */
+    public function isEscapeCss();
+
+    /**
+     * Determine if javascript escaping strategy is applied.
+     *
+     * @param bool $escapeJs
+     */
+    public function setEscapeJs($escapeJs = true);
+
+    /**
+     * Check if javascript escaping strategy is applied.
+     *
+     * @return bool
+     */
+    public function isEscapeJs();
+
+    /**
+     * Determine if urls escaping strategy is applied.
+     *
+     * @param bool $escapeUrl
+     */
+    public function setEscapeUrl($escapeUrl = true);
+
+    /**
+     * Check if urls escaping strategy is applied.
+     *
+     * @return bool
+     */
+    public function isEscapeUrl();
 }
